70b11582eaf4ce0905e43cacee9332a7b4641271
KaviduAloka <aloka94@hotmail.com>
2025-09-17 00:14:07 +0530

    [chore] setup theme in application by a global state

--- App.tsx
+++ App.tsx
@@ -5,25 +5,25 @@
  * @format
  */
 
-import { useColorScheme } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from 'react-redux';
 import Feature1 from './src/features/Feature1';
 import Feature2 from './src/features/Feature2';
 import { NavigationContainer } from '@react-navigation/native';
 import { store } from './src/store/reducers';
+import { ThemeProvider } from './src/components/ThemeContext';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-        </Stack.Navigator>
-      </NavigationContainer>
+      <ThemeProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
     </Provider>
   );
 }
---
+++ src/assets/themes.tsx
@@ -0,0 +1,13 @@
+import colors from './colors';
+
+export const lightTheme = {
+  mode: 'light',
+  background: colors.WHITE,
+  text: colors.BLACK,
+};
+
+export const darkTheme = {
+  mode: 'dark',
+  background: colors.BLACK,
+  text: colors.WHITE,
+};
